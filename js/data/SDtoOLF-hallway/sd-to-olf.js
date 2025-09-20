af37b8b2b9097bc0af5a09ff8f59c8f962028cc0
LanzMallari <lanzmallari.edu@gmail.com>
2025-09-20 18:43:18 +0800

    2nd commit hallway

--- js/data/SDtoOLF-hallway/sd-to-olf.js
+++ js/data/SDtoOLF-hallway/sd-to-olf.js
@@ -1,18 +1,28 @@
 const sdtoolbldg =[
     {
     id: 'SDtoOLF1',
-    name: 'SCS 101',
-    description: 'SCS bldg. 1st floor',
+    name: 'Sd to OLF hallway1',
+    description: 'SD to OLF Hallway',
     imageUrl: 'images/SDtoOlf-Hallway/connectingsdolf1.jpg',
-    category: ['SCS bldg.'],
-    connections: ['SdGroundfloor2','scs1st2'],
+    category: ['hallway'],
+    connections: ['SdGroundfloor10','SDtoOLF2'],
     arrowPositions: {
-      'SdGroundfloor2': { phi: Math.PI / 3, theta: Math.PI * 2.05, direction: 'up'},
-      'scs1st2': { phi: Math.PI / 3, theta: Math.PI * 1.15, direction: 'up'}
-
-        
+      'SDtoOLF2': { phi: Math.PI / 3, theta: Math.PI * 2.05, direction: 'up'},
+      'SdGroundfloor10': { phi: Math.PI / 3, theta: Math.PI * 1, direction: 'up'}
        }
     },
 
+ {
+    id: 'SDtoOLF2',
+    name: 'Sd to OLF hallway2',
+    description: 'SD to OLF Hallway',
+    imageUrl: 'images/SDtoOlf-Hallway/connectingsdolf1.jpg',
+    category: ['hallway'],
+    connections: ['SDtoOLF1','scs1st2'],
+    arrowPositions: {
+      'SdGroundfloor2': { phi: Math.PI / 3, theta: Math.PI * 1.8  , direction: 'up'},
+      'SDtoOLF1': { phi: Math.PI / 3, theta: Math.PI * 1.15, direction: 'up'}
+       }
+    },
 
 ];
