693577137bf6c1e9126c30ce9a9ec6c8961e5f09
Mark Edrian S. Talavera <124575322+markedrianst@users.noreply.github.com>
2025-09-21 23:15:12 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // Load initial panorama (entrance)
 
-    window.panoramaViewer.loadPanorama('SdGroundfloor10');
+    window.panoramaViewer.loadPanorama('shortcutsdtoccs');
 
 
 
