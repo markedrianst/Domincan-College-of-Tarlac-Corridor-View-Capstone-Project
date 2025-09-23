45593b60d52256238f516222fd026d1d61f9a591
markedrianst <markedriantalavera070@gmail.com>
2025-09-23 22:57:59 +0800

    Update floor descriptions and categories for consistency across HR and OLP buildings

--- js/data/HRbldg/hr1stfloor.js
+++ js/data/HRbldg/hr1stfloor.js
@@ -3,7 +3,7 @@ const hr1stfloor = [
  {  //connects to scs2nd to scs3rd
     id: 'shortcutsdtoccs',
     name: 'ShortCuttoCCS',
-    description: 'HR bldg. Ground Floor',
+    description: 'HR bldg. 1st floor',
     imageUrl: 'images/HRbldg/sdShortcutToDepartmentOffices.jpg',
     category: ['HR bldg.'],
     connections: ['SdGroundfloor10','Hr1rd1'],
@@ -16,9 +16,9 @@ const hr1stfloor = [
     {  //connects to scs2nd to scs3rd
     id: 'Hr1rd1',
     name: 'CCS OFFICE',
-    description: 'HR bldg. Ground Floor',
+    description: 'HR bldg. 1st floor',
     imageUrl: 'images/HRbldg/HR1stfloor2.jpg',
-    category: ['HR bldg.','Offices'],
+    category: ['HR bldg.'],
     connections: ['shortcutsdtoccs','Hr1rd2','sn1st1'],
     arrowPositions: {
         'shortcutsdtoccs': { phi: Math.PI / 3, theta: Math.PI * .9, direction: 'up'},
@@ -31,9 +31,9 @@ const hr1stfloor = [
 {  //connects to scs2nd to scs3rd
     id: 'Hr1rd2', 
     name: 'CLA/CED OFFICE',
-    description: 'HR bldg. Ground Floor',
+    description: 'HR bldg. 3rd floor',
     imageUrl: 'images/HRbldg/HR1stfloor1.jpg',
-    category: ['HR bldg.','Offices'],
+    category: ['HR bldg.'],
     connections: ['Hr1rd1','Hr1rd3'],
     arrowPositions: {
         'Hr1rd1': { phi: Math.PI / 3, theta: Math.PI * .92, direction: 'up'},
@@ -43,27 +43,14 @@ const hr1stfloor = [
 },
 
 
-{  //connects to scs2nd to scs3rd
-    id: 'Hr1rd3',
-    name: 'BSHM OFFICE',
-    description: 'HR bldg. Ground Floor',
-    imageUrl: 'images/HRbldg/HR1stfloor3.jpg',
-    category: ['HR bldg.','Offices'],
-    connections: ['Hr1rd2','Hr1rd4'],
-    arrowPositions: {
-        'Hr1rd2': { phi: Math.PI / 3, theta: Math.PI * .9, direction: 'up'},
-        'Hr1rd4': { phi: Math.PI / 3, theta: Math.PI * 2.1, direction: 'up'}
-
-       }
-},
 
 
 {  //connects to scs2nd to scs3rd
     id: 'Hr1rd4',
     name: 'CBA,CCJE',
-    description: 'HR bldg. Ground Floor',
+    description: 'HR bldg. 1st floor',
     imageUrl: 'images/HRbldg/HR1stfloor4.jpg',
-    category: ['HR bldg.','Offices'],
+    category: ['HR bldg.'],
     connections: ['Hr1rd3','Hr2nd2','Hr1rd5'],
     arrowPositions: {
         'Hr1rd3': { phi: Math.PI / 3, theta: Math.PI * .9, direction: 'up'},
@@ -73,11 +60,24 @@ const hr1stfloor = [
        }
 },
 
+{  //connects to scs2nd to scs3rd
+    id: 'Hr1rd3',
+    name: 'BSHM OFFICE',
+    description: 'HR bldg. 3rd floor',
+    imageUrl: 'images/HRbldg/HR1stfloor3.jpg',
+    category: ['HR bldg.'],
+    connections: [,'Hr1rd4','Hr1rd2'],
+    arrowPositions: {
+      
+        'Hr1rd4': { phi: Math.PI / 3, theta: Math.PI * 2.1, direction: 'up'},
+          'Hr1rd2': { phi: Math.PI / 3, theta: Math.PI * 0.85, direction: 'up'},
 
+       }
+},
 {  //connects to scs2nd to scs3rd
     id: 'Hr1rd5',
     name: 'Center Hall',
-    description: 'HR bldg. Ground Floor',
+    description: 'HR bldg. 3rd floor',
     imageUrl: 'images/HRbldg/HR1stfloor5.jpg',
     category: ['HR bldg.'],
     connections: ['Hr1rd4','Hr1rd6','SDtoOLF4','connectinghallwaytolp'],
@@ -94,7 +94,7 @@ const hr1stfloor = [
 {  //connects to scs2nd to scs3rd
     id: 'Hr1rd6',
     name: 'Center Hall',
-    description: 'HR bldg. GroundFloor',
+    description: 'HR bldg. 3rd floor',
     imageUrl: 'images/HRbldg/HR1stfloor6.jpg',
     category: ['HR bldg.'],
     connections: ['Hr1rd5','Hr3rd2'],
@@ -103,5 +103,6 @@ const hr1stfloor = [
       
        }
 },
+
 
 ];
