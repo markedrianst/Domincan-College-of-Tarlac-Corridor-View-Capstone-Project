a4a59a383917e215ad24fc8f6741374161df4ce1
markedrianst <markedriantalavera070@gmail.com>
2025-10-04 14:54:07 +0800

    change the  default load

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // Load initial panorama (entrance)
 
-    window.panoramaViewer.loadPanorama('scs1stStairsConnection');
+    window.panoramaViewer.loadPanorama('entrance');
 
 
 
